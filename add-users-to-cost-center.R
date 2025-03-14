159e68dd88dcc9f5f66c637b44b723d3fedfc58f
Kathryn Doering <kathryn.doering@noaa.gov>
2025-03-14 15:29:39 -0700

    example of adding for each FMC

--- add-users-to-cost-center.R
+++ add-users-to-cost-center.R
@@ -7,6 +7,7 @@
 library(gitcreds)
 library(readxl)
 library(gh)
+library(dplyr)
 # library(httr)
 
 ## Set up credentials ----
@@ -14,13 +15,27 @@ library(gh)
 # use gitcreds::gitcreds_get() to check gh token credentials.
 # Use gitcreds::gitcreds_set() to update the credentials if needed
 
-## Get usernames ----
+## Get all GHEC usernames ----
+all_GHEC_users <- read_excel("all_users.xlsx")
+
+
+## Get list of usernames to add to a cost center ----
 
 # example with usernames in the column of a spreadsheet
 # Get the names of people to add to the cost center (not)
-ost_names <- read_excel("ost_names.xlsx")
+filename <- "nefsc_names.xlsx"
+office_usernames <- read_excel(filename)
+
+#check who is found
+#intersect(office_usernames, all_GHEC_users)
+# check who is missing; unfortuantely is case sensitive
+not_included <- setdiff(office_usernames, all_GHEC_users)
+
 #vector of names
-ppl_to_add <- ost_names$usernames
+
+ppl_to_add <- intersect(office_usernames, all_GHEC_users) 
+ppl_to_add <- ppl_to_add$usernames
+
 
 # example with 2 users
 #ppl_to_add <- c("github-username-1", "github-username-2")
@@ -31,10 +46,20 @@ ppl_to_add <- ost_names$usernames
 
 cost_centers <- gh("GET /enterprises/{enterprise}/settings/billing/cost-centers", enterprise = "NOAA-NMFS")
 # will need to dig more into parsing this structure when there are multiple cost centers
-ost_id <- cost_centers[[1]][[1]]$id
+id <- cost_centers[[1]][[5]]$id
+
+## verify users part of enterprise ---
+
+
 
 ## Add user(s) to a cost center ----
+# Note: can only do a mx of 50 users at a time
+length(ppl_to_add) #check less than 50, otherwise run in chunks
 
 cost_centers <- gh(
   "POST /enterprises/{enterprise}/settings/billing/cost-centers/{cost_center_id}/resource", 
-  enterprise = "NOAA-NMFS", cost_center_id = ost_id, users = as.array(ppl_to_add))
+  enterprise = "NOAA-NMFS", cost_center_id = id, users = as.array(ppl_to_add[101:148]))
+
+# check added
+cost_centers <- gh("GET /enterprises/{enterprise}/settings/billing/cost-centers", enterprise = "NOAA-NMFS")
+
